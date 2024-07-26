3fbaa0ee52a634eaa316c1a04fd237a8b40c3bcb
Basith P <basithpofficial@gmail.com>
2024-07-26 17:20:33 +0530

    start project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.my_news
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:my_news/app.dart';
+import 'package:my_news/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
